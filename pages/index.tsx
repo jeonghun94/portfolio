cebfbd78cd13315c5c980d783edd22090191604f
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-06-20 17:24:16 +0900

    tailwindcss 테스팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-100">
-      <h1 className="text-blue-300">Hello Next.js</h1>
+    <div className="bg-red-500 w-1/2 h-full flex items-center gap-2">
+      <h1 className="text-blue-500 justify-center">Hello Next.js</h1>
+      <div className="aspect-square w-4 h-4 bg-slate-500 "></div>
     </div>
   );
 };
